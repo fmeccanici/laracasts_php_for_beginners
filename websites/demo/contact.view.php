e2cbb3d505efcfe6f099c318963b833166047f86
fmeccanici <florismeccanici@mecctech-solutions.nl>
2023-09-29 19:29:18 +0200

    Ep 12 finished

--- websites/demo/contact.view.php
+++ websites/demo/contact.view.php
@@ -118,12 +118,12 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                Hello. Welcome to the page.
+                Now on the contact page.
             </div>
         </main>
     </div>
