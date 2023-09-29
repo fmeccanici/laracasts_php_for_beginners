98c69d5965d640aff969182e806922a992dbc284
fmeccanici <florismeccanici@mecctech-solutions.nl>
2023-09-29 18:20:56 +0200

    Ep 6 finished

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -3,28 +3,21 @@
 <head>
     <meta charset="utf-8">
     <title>Demo</title>
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            font-family: sans-serif;
-        }
-    </style>
 </head>
 <body>
-    <?php
-        $name = "Dark Matter";
-        $read = true;
+    <h1>Recommended Books</h1>
 
-        if ($read)
-        {
-            $message = "You have read \"$name\"";
-        } else {
-            $message = "You haven't read \"$name\"";
-        }
+    <?php
+        $books = [
+            "Do Androids Dream of Electric Sheep",
+            "The Langoliers",
+            "Hail Mary"
+        ];
     ?>
-    <h1>
-        <?= $message ?>
-    </h1>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li><?= $book ?></li>
+        <?php endforeach; ?>
+    </ul>
 </body>
+</html>
