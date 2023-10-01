ad5674abb0750d861b12074d8d06e6276e95f181
fmeccanici <florismeccanici@mecctech-solutions.nl>
2023-10-01 19:19:38 +0200

    Ep 17 finished

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,3 +1,15 @@
 <?php
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+// connect to our MySQL database using the PDO extension
+$dsn = "mysql:host=localhost;port=3306;dbname=demo;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($posts as $post)
+{
+    echo "<li>{$post['title']}</li>";
+}
