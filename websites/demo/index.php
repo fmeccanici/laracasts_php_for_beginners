ac8cca0de0000d7fb2094a7f5501c99a904ad230
fmeccanici <florismeccanici@mecctech-solutions.nl>
2023-10-01 21:19:41 +0200

    Ep 23 finished

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,18 +1,4 @@
 <?php
-require 'functions.php';
 require 'Database.php';
-
-// Connect to the database, and execute a query.
-$config = require('config.php');
-
-$db = new Database($config['database']);
-$id = $_GET['id'];
-$query = "select * from posts where :id = id";
-
-$posts = $db->query($query, [':id' => $id])->fetchAll();
-
-foreach ($posts as $post)
-{
-    echo "<li>{$post['title']}</li>";
-}
-
+require 'functions.php';
+require 'router.php';
--- websites/demo/router.php
+++ websites/demo/router.php
@@ -5,6 +5,8 @@ $routes = [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
     '/contact' => 'controllers/contact.php',
+    '/notes' => 'controllers/notes.php',
+    '/note' => 'controllers/note.php',
 ];
 
 /**
