b16eb4ee4c7ecea7c7bc4da61228582af3fcd03d
fmeccanici <florismeccanici@mecctech-solutions.nl>
2023-10-02 22:08:46 +0200

    Ep 23 finished

--- websites/demo/controllers/note.php
+++ websites/demo/controllers/note.php
@@ -10,4 +10,16 @@ $note = $db->query('select * from notes where id = :id', [
     'id' => $id
 ])->fetch();
 
+if (! $note)
+{
+    abort();
+}
+
+$currentUserId = 1;
+
+if ($note['user_id'] !== $userId)
+{
+    abort(Response::FORBIDDEN);
+}
+
 require "views/note.view.php";
