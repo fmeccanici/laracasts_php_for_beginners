56cf8783259066536beb06c8bbb905b3c33fe36c
fmeccanici <florismeccanici@mecctech-solutions.nl>
2023-10-04 22:08:12 +0200

    Ep. 27 finished

--- websites/demo/views/note.view.php
+++ websites/demo/views/note.view.php
@@ -1,12 +1,11 @@
 <?php require "partials/head.php"; ?>
 <?php require "partials/banner.php"; ?>
-
-        <main>
-            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <p>
-                    <a class="text-blue-500 hover:underline" href="/notes">Go back...</a>
-                </p>
-                <p><?=$note['body'] ?></p>
-            </div>
-        </main>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>
+            <a class="text-blue-500 hover:underline" href="/notes">Go back...</a>
+        </p>
+        <p><?= htmlspecialchars($note['body'])?></p>
+    </div>
+</main>
 <?php require "partials/footer.php"; ?>
