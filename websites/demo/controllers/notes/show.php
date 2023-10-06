a41c90d5c3f7720b4241500f3f1a6cda09756d9d
fmeccanici <florismeccanici@mecctech-solutions.nl>
2023-10-06 13:39:21 +0200

    Ep. 32 finished

--- websites/demo/controllers/notes/show.php
+++ websites/demo/controllers/notes/show.php
@@ -5,17 +5,36 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$id = $_GET['id'];
-
-$note = $db->query('select * from notes where id = :id', [
-    'id' => $id
-])->findOrFail();
-
 $currentUserId = 1;
 
-authorize($note['user_id'] === $currentUserId);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_GET['id'];
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $id
+    ])->findOrFail();
+
+
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query('delete from notes where id = :id', [
+        'id' => $_POST['id']
+    ]);
+
+    header('location: /notes');
+} else {
+    $id = $_GET['id'];
+
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $id
+    ])->findOrFail();
+
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
+
--- websites/demo/views/notes/show.view.php
+++ websites/demo/views/notes/show.view.php
@@ -6,6 +6,11 @@
             <a class="text-blue-500 hover:underline" href="/notes">Go back...</a>
         </p>
         <p><?= htmlspecialchars($note['body'])?></p>
+
+        <form class="mt-6" method="POST">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
+            <button class="text-sm text-red-500">Delete</button>
+        </form>
     </div>
 </main>
 <?php require base_path("views/partials/footer.php"); ?>
