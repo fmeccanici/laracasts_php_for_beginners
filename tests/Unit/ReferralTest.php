e260514c4a7283ef9d896973fa2d48c6baa6b170
fmeccanici <florismeccanici@mecctech-solutions.nl>
2023-10-08 12:19:57 +0200

    Ep. 49 finished

---
+++ tests/Unit/ReferralTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Core\Container;
+
+it('allows subscribers to earn money by referring their friends', function () {
+    // Referral
+    // redeem
+});
+
+it('disallowed guests from participating in the referral system', function () {
+
+});
